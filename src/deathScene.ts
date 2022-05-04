89b98194fc93a6a6303c16f848b6fc40b47e95b2
jonassaa <47302524+jonassaa@users.noreply.github.com>
2022-05-04 10:49:54 +0200

    Revert "added gamepad reset, text description and styling"
    This reverts commit 165f8579314ecb2325de1e900a836242315c117a.

--- src/deathScene.ts
+++ src/deathScene.ts
@@ -1,9 +1,8 @@
-import { BUTTON_SPACING, BUTTON_STYLE, GAME_OVER_TEXT_STYLE, INFO_TEXT_STYLE, newButton, PAUSE_TEXT_STYLE } from "./utils";
+import { BUTTON_STYLE, GAME_OVER_TEXT_STYLE, newButton } from "./utils";
 
 const windowHeight = window.innerHeight;
 const windowWidth = window.innerWidth;
-
-let playing = false;
+const BUTTON_SIDE_OFFSET = 100;
 
 export class DeathScene extends Phaser.Scene {
 
@@ -18,14 +17,15 @@ export class DeathScene extends Phaser.Scene {
     public create() {
         this.cameras.main.setBackgroundColor("rgba(255, 0, 0, 0.5)");
 
-        // Death menu frame
-        const deathMenuFrame = this.add.rectangle(windowWidth / 2, windowHeight / 2 - 50, 350, 200, 0xe66a63);
+        // GAME OVER text
+        const gameOverText = this.add.text(
+            windowWidth / 2,
+            windowHeight / 2 - BUTTON_SIDE_OFFSET,
+            "GAME OVER",
+            GAME_OVER_TEXT_STYLE,
+            );
 
-        deathMenuFrame.setStrokeStyle(10, 0xffffff);
-
-        // Game over text
-        const deathText = this.add.text(windowWidth / 2, windowHeight / 2 - 150, "GAME OVER", PAUSE_TEXT_STYLE);
-        deathText.setX(deathText.x - deathText.width / 2);
+        gameOverText.setX(gameOverText.x - gameOverText.width / 2);
 
         // New game button
         newButton(this, "New Game",
@@ -34,42 +34,7 @@ export class DeathScene extends Phaser.Scene {
             this.scene.launch("game");
             this.scene.setVisible(false);
         },
-        windowWidth / 2,
-        windowHeight / 2 - 40,
-        BUTTON_STYLE,
-        );
-
-        // Gamepad hints
-        const hintText = this.add.text(
-            deathMenuFrame.x ,
-            deathMenuFrame.y + deathMenuFrame.height / 2 - 50 ,
-            "Push SELECT to restart" ,
-            {
-                backgroundColor: "rgba(0,0,0,0)",
-                color: "#ffffff",
-                font: "25px monospace",
-                padding: {
-                    x: 20,
-                    y: 10,
-                },
-            },
-            );
-        hintText.setX(hintText.x - hintText.width / 2);
-    }
-
-    public update() {
-        const pad = this.input.gamepad.pad1;
-
-        if (pad && pad.isButtonDown(8) && !playing) {
-            this.scene.pause();
-            this.scene.launch("game");
-            this.scene.setVisible(false);
-            playing = true;
-        }
-
-        if (pad && !pad.isButtonDown(8)) {
-            playing = false;
-        }
+        windowWidth / 2, windowHeight / 2 + BUTTON_SIDE_OFFSET, BUTTON_STYLE);
     }
 
 }
