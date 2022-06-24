ef36e5543fb11d0a3f55adc07b4666bd6425a798
Jonas Strand Aasberg <jonas.aasberg@clave.no>
2022-06-24 09:58:55 +0200

    input form changes

--- src/finishScene.ts
+++ src/finishScene.ts
@@ -1,6 +1,8 @@
-import { NumberField, TextField } from "./models/inputField";
+import { MenuDirection } from "./models/direction";
+import { InputForm } from "./models/inputField";
+import { MenuButton, NavMenu } from "./models/navMenu";
 import { IPlayerInfo } from "./models/playerInfo";
-import { BUTTON_STYLE, getSelectedLevel, newButton, PAUSE_TEXT_STYLE, recordTime } from "./utils";
+import { getSelectedLevel, PAUSE_TEXT_STYLE, recordTime } from "./utils";
 
 const windowHeight = window.innerHeight;
 const windowWidth = window.innerWidth;
@@ -10,145 +12,87 @@ const playerInfo: IPlayerInfo = {
     phone: "",
     time: "",
 };
-let nameField: TextField;
-let phoneField: NumberField;
-let submitted = false;
-let submitButton: Phaser.GameObjects.DOMElement;
-let playing = false;
-
 export class FinishScene extends Phaser.Scene {
     private stars: number;
+
     private timer: number;
-    private tabKey: Phaser.Input.Keyboard.Key;
+
+    private inputForm: InputForm;
+
+    private menu: NavMenu;
 
     constructor(config: Phaser.Types.Scenes.SettingsConfig) {
         super(config);
     }
+
     public init(data: { time: number; stars: number; }) {
         this.timer = data.time;
         this.stars = data.stars;
+    }
+
+    public preload() {
+        this.load.html(
+            "playerInfoForm", "assets/common/inputForm.html",
+        );
     }
 
     public create() {
         this.cameras.main.setBackgroundColor("rgba(120, 120, 120, 0.5)");
 
         // Finish menu frame
-        const finishMenuFrame = this.add.rectangle(windowWidth / 2, windowHeight / 2 - 15, 650, 575, 0xb1bd9b);
-
-        finishMenuFrame.setStrokeStyle(10, 0xffffff);
+        const finishMenuFrame = this.add.rectangle(
+            windowWidth / 2, windowHeight / 2 - 15, 650, 575, 0xb1bd9b,
+        );
+        finishMenuFrame.setStrokeStyle(
+            10, 0xffffff,
+        );
 
         // Finish text
-        const finishText = this.add.text(windowWidth / 2, windowHeight / 2 - 300, "CONGRATULATIONS", PAUSE_TEXT_STYLE);
+        const finishText = this.add.text(
+            windowWidth / 2, windowHeight / 2 - 300, "CONGRATULATIONS", PAUSE_TEXT_STYLE,
+        );
         finishText.setX(finishText.x - finishText.width / 2);
 
-        submitted = false;
-
         // User input form
-        nameField = new TextField(this,
-            finishMenuFrame.getBottomLeft().x + 50,
-            windowHeight / 2 - 2 * BUTTON_SIDE_OFFSET,
-            finishMenuFrame.width - 100,
-            "Name/Nickname:",
-            BUTTON_STYLE);
+        this.inputForm = new InputForm(
+            this, windowWidth / 2, windowHeight / 2 - 100, "playerInfoForm",
+        );
 
-        const inputHintText = this.add.text(
-            finishMenuFrame.x ,
-            finishMenuFrame.y + finishMenuFrame.height / 2 - 425 ,
-            "Press TAB to switch between fields",
-            {
-                backgroundColor: "rgba(0,0,0,0)",
-                color: "#ffffff",
-                font: "25px monospace",
-                padding: {
-                    x: 20,
-                    y: 10,
-                },
-            },
-            );
-        inputHintText.setX(inputHintText.x - inputHintText.width / 2);
+        const submitButton = new MenuButton(
+            windowWidth / 2, windowHeight / 2 + 50, "Submit", () => {
+                const selectedLevel = getSelectedLevel();
+                playerInfo.name = this.inputForm.getName();
+                playerInfo.phone = this.inputForm.getPhone();
+                playerInfo.time = String(this.timer.toFixed(2));
+                if (!!selectedLevel) {
+                    recordTime(
+                        this.stars, this.timer, playerInfo.name, playerInfo.phone, selectedLevel,
+                    );
+                }
+                this.scene.pause();
+                this.scene.launch(
+                    "leaderboard", { fromMenu: false, currentPlayer: playerInfo },
+                );
+                this.scene.setVisible(false);
+                this.menu.destroy();
+                this.inputForm.destroy();
+            }, this,
+        );
 
-        phoneField = new NumberField(this,
-            finishMenuFrame.getBottomLeft().x + 50,
-            windowHeight / 2 - BUTTON_SIDE_OFFSET,
-            finishMenuFrame.width - 100,
-            "Phone number:",
-            BUTTON_STYLE);
+        const newGameButton = new MenuButton(
+            windowWidth / 2, windowHeight / 2 + BUTTON_SIDE_OFFSET + 50, "New Game", () => {
+                this.scene.pause();
+                this.scene.launch(
+                    "leaderboard", { fromMenu: false, currentPlayer: playerInfo },
+                );
+                this.scene.setVisible(false);
+                this.menu.destroy();
+                this.inputForm.destroy();
+            }, this,
+        );
 
-        nameField.activate();
-
-        this.tabKey = this.input.keyboard.addKey('TAB', true);
-        
-        this.input.keyboard.on("keydown-TAB", () => {
-            nameField.switchActive();
-            phoneField.switchActive();
-        });
-
-        // Submit Button
-        submitButton = newButton(this, "Submit",
-        () => {
-            const selectedLevel = getSelectedLevel();
-            playerInfo.name = nameField.getValue();
-            playerInfo.phone = phoneField.getValue();
-            playerInfo.time = String(this.timer.toFixed(2));
-            nameField.clear();
-            phoneField.clear();
-            if (!!selectedLevel){
-                recordTime(this.stars, this.timer, playerInfo.name, playerInfo.phone, selectedLevel);
-            }
-            submitted = true;
-            this.scene.pause();
-            this.scene.launch("leaderboard", { fromMenu: false ,currentPlayer: playerInfo});
-            this.scene.setVisible(false);
-
-        },
-        windowWidth / 2, windowHeight / 2 , BUTTON_STYLE);
-
-        // Gamepad hints
-        const hintText = this.add.text(
-            finishMenuFrame.x ,
-            finishMenuFrame.y + finishMenuFrame.height / 2 - 50 ,
-            "Push SELECT to restart",
-            {
-                backgroundColor: "rgba(0,0,0,0)",
-                color: "#ffffff",
-                font: "25px monospace",
-                padding: {
-                    x: 20,
-                    y: 10,
-                },
-            },
-            );
-        hintText.setX(hintText.x - hintText.width / 2);
-
-        // New game button
-        newButton(this, "New Game",
-        () => {
-            this.scene.pause();
-            this.scene.launch("leaderboard", { fromMenu: false ,currentPlayer: playerInfo});
-            this.scene.setVisible(false);
-        },
-        windowWidth / 2, windowHeight / 2 + BUTTON_SIDE_OFFSET, BUTTON_STYLE);
+        this.menu = new NavMenu(
+            [submitButton, newGameButton], MenuDirection.Vertical, this,
+        );
     }
-
-    public update() {
-        const pad = this.input.gamepad.pad1;
-
-        if (pad && pad.isButtonDown(8) && !playing) {
-            this.scene.pause();
-            this.scene.launch("leaderboard", { fromMenu: false });
-            this.scene.setVisible(false);
-            playing = true;
-        }
-
-        if (pad && !pad.isButtonDown(8)) {
-            playing = false;
-        }
-
-        if (submitted && submitButton) {
-            submitButton.destroy();
-            nameField.deactivate();
-            phoneField.deactivate();
-        }
-    }
-
 }
--- src/models/inputField.ts
+++ src/models/inputField.ts
@@ -1,35 +1,43 @@
 export class InputForm {
     public value: string;
+
     public form: Phaser.GameObjects.DOMElement;
+
     private nameValue: string;
+
     private phoneValue:string;
+
     constructor(
         parent: Phaser.Scene,
         posX: number,
         posY: number,
         htmlRef: string,
-        ) {
-        this.form = parent.add.dom(posX,posY).createFromCache(htmlRef);
+    ) {
+        this.form = parent.add.dom(
+            posX, posY,
+        ).createFromCache(htmlRef);
 
         this.form.addListener("input");
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        this.form.on("input", (event: {target:any;}) => {
-            if (event.target.name == "name") {
-                this.setName(event.target.value)
-            } else if (event.target.name == "phone") {
-                this.setPhone(event.target.name)
-            }
-        })
-
+        this.form.on(
+            "input", (event: { target:any; }) => {
+                if (event.target.name == "name") {
+                    this.setName(event.target.value);
+                } else if (event.target.name == "phone") {
+                    this.setPhone(event.target.value);
+                }
+            },
+        );
     }
+
     private setPhone(value:string) {
         this.phoneValue = value;
-    } 
+    }
 
     private setName(value:string) {
         this.nameValue = value;
     }
-    
+
     public getName() {
         return this.nameValue;
     }
