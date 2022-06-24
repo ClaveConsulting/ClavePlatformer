f29a0bd1b667468a50eb6bc20bcb3f725f891b0c
Jonas Strand Aasberg <jonas.aasberg@clave.no>
2022-06-24 10:00:21 +0200

    testScene

--- src/testScene.ts
+++ src/testScene.ts
@@ -1,69 +1,44 @@
-import { MenuDirection } from "./models/direction";
-import { MenuButton, MenuImage, NavMenu } from "./models/navMenu";
-import { BUTTON_SPACING, BUTTON_STYLE, INFO_TEXT_STYLE, newButton, PAUSE_TEXT_STYLE } from "./utils";
-
-
+import { InputForm } from "./models/inputField";
+import { NavMenu } from "./models/navMenu";
 
 const windowHeight = window.innerHeight;
 const windowWidth = window.innerWidth;
-let playing = false;
 
 export class TestScene extends Phaser.Scene {
     private menu:NavMenu;
+
+    private form: InputForm;
 
     constructor(config: Phaser.Types.Scenes.SettingsConfig) {
         super(config);
     }
 
-    public preload(){
-        this.load.image("test", "assets/common/tileset.png")
-        this.load.css("nes-btn","assets/common/nesCss.css")
+    public preload() {
+        this.load.image(
+            "test", "assets/common/tileset.png",
+        );
+        this.load.css(
+            "nes-btn", "assets/common/nesCss.css",
+        );
+        this.load.html(
+            "nameField", "assets/common/inputField.html",
+        );
     }
 
     public create() {
-
         this.cameras.main.setBackgroundColor("rgba(120, 120, 120, 0.5)");
 
         // Pause menu frame
-        const pauseMenuFrame = this.add.rectangle(windowWidth / 2, windowHeight / 2 - 50, 250, 400, 0xb1bd9b);
+        const pauseMenuFrame = this.add.rectangle(
+            windowWidth / 2, windowHeight / 2 - 50, 250, 400, 0xb1bd9b,
+        ).setInteractive();
 
-        pauseMenuFrame.setStrokeStyle(10, 0xffffff);
-        
-        let item1 = new MenuButton(100,100,"TEST",() => {
-            this.scene.pause();
-            this.scene.launch("leaderboard", {fromMenu: true});
-            this.scene.setVisible(false);
-            this.menu.destroy()
+        pauseMenuFrame.setStrokeStyle(
+            10, 0xffffff,
+        );
 
-        },this)
-
-        let item2 = new MenuButton(windowWidth/2,windowHeight/2,"TEST0000000",() => {
-            this.scene.pause();
-            this.scene.launch("leaderboard", {fromMenu: true});
-            this.scene.setVisible(false);
-            this.menu.destroy()
-
-        },this)
-        console.log(item2.target.displayWidth)
-        let item3 = new MenuButton(100,240,"TEST",() => {
-            this.scene.pause();
-            this.scene.launch("leaderboard", {fromMenu: true});
-            this.scene.setVisible(false);
-            this.menu.destroy()
-
-        },this)
-
-        let image1 = new MenuImage(windowWidth/2,windowHeight/2,"test",0.3,() => console.log("image1 Pressed"),this);
-        let image2 = new MenuImage(600,200,"test",0.2,() => console.log("image2 Pressed"),this);
-
-        this.menu = new NavMenu([item1,item2,item3,image2],MenuDirection.Vertical,this);
-
-        let imageMenu = new NavMenu([image1,image2],MenuDirection.Horizontal,this);
+        this.form = new InputForm(
+            this, windowWidth / 2, windowHeight / 2, "nameField",
+        );
     }
-
-    public update() {
-        const pad = this.input.gamepad.pad1;
-
-    }
-
 }
