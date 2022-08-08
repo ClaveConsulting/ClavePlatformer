006d1b910fedb2ef935109003a2f9de72d0a09f2
Jonas Strand Aasberg <jonas.aasberg@clave.no>
2022-08-08 09:07:56 +0200

    fix hotload game

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,43 +1,44 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: "./src/app.ts",
-  devtool: "source-map",
-  output: {
-    filename: "./bundle.js",
-    clean: true,
-  },
-  resolve: {
-    extensions: [".ts"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: "ts-loader",
-      },
-      {
-        test: /\.(png|json|ttf|xhtml)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Output Management",
-      template: "index.html",
-    }),
-  ],
-  devServer: {
-    https: true,
-    static: {
-      directory: path.join(__dirname, "dist"),
+    entry: "./src/app.ts",
+    devtool: "source-map",
+    mode: "development",
+    output: {
+        filename: "./bundle.js",
+        clean: true,
     },
-    compress: true,
-    port: 9000,
-  },
+    resolve: {
+        extensions: [".wasm", ".ts", ".tsx", ".mjs", ".cjs", ".js", ".json"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+            },
+            {
+                test: /\.(png|json|ttf|xhtml)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Output Management",
+            template: "index.html",
+        }),
+    ],
+    devServer: {
+        https: true,
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        compress: true,
+        port: 9000,
+    },
 };
